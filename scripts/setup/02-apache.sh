6b81a3be006b7c159c3b79b99003fbb77b0dd720
Kris Jordan <krisjordan@gmail.com>
2009-08-17 16:33:19 -0400

    Removing yes from aptitude commands as it seems to halt progress. Using -y option instead.

--- scripts/setup/02-apache.sh
+++ scripts/setup/02-apache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yes | sudo aptitude install apache2 apache2-mpm-prefork apache2-prefork-dev apache2-utils apache2.2-common
+sudo aptitude install -y apache2 apache2-mpm-prefork apache2-prefork-dev apache2-utils apache2.2-common
--- scripts/setup/08-xdebug.sh
+++ scripts/setup/08-xdebug.sh
@@ -2,10 +2,10 @@
 SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR/../../
 DEVENV=`pwd`
-yes | sudo aptitude install kcachegrind
+sudo aptitude install -y kcachegrind
 mkdir "$DEVENV/tools/xdebug"
 chmod 0777 "$DEVENV/tools/xdebug"
-yes | sudo pecl install xdebug
+sudo pecl install xdebug
 cat "$DEVENV/scripts/setup/etc/php/xdebug.ini" | sudo tee -a /usr/local/lib/php.ini
 sudo "$DEVENV/scripts//util/string-replace.sh" /usr/local/lib/php.ini "__DEVENV__" "$DEVENV"
 sudo /etc/init.d/apache2 restart
