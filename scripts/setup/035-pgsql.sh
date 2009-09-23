e61019900839106ec5bcdd255f42b519fd4223f5
Ryan <ryan@ryan-laptop.(none)>
2009-09-23 09:13:59 -0400

    Added postgres installation

---
+++ scripts/setup/035-pgsql.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo aptitude install -y postgresql-8.3 postgresql-client-8.3 postgresql-contrib-8.3 postgresql-server-dev-8.3
--- scripts/setup/install-sandbox.sh
+++ scripts/setup/install-sandbox.sh
@@ -4,6 +4,7 @@ cd $SCRIPTS_DIR
 ./01-prereqs.sh
 ./02-apache.sh
 ./03-mysql.sh
+./035-pgsql.sh
 ./04-php-5-3.sh
 ./05-ubuntu-apache-php-conf.sh
 ./06-pdt.sh
